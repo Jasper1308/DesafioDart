2c86d3b0d1b5436d588e80049f2e8e619e2840b1
Jasper1308 <adrianpjasper@gmail.com>
2025-04-21 04:21:02 -0300

    Primeiro commit, Definição de pastas, Criação de Classe MedidaClimatica, Funções para formatar dados CSV

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+import 'package:desafioDart/functions.dart';
+import 'package:desafioDart/class.dart';
+
+void main() async{
+  // Lê todos os arquivos do diretório
+  final arquivos = Directory(r'C:\clima\sensores').listSync();
+  // Lista para adicionar todos arquivos CSV como String
+  List<String> listaArquivos = await paraString(arquivos);
+  // Lista para armazenar os dados como objetos MedidaClimatica
+  Map<String, List<MedidaClimatica>> dados = await preencherMapa(listaArquivos, arquivos);
+  print(dados);
+}
